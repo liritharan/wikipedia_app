b0dff010e301964d70c53d1416b9376ed26707d1
liritharan <lirininfo@gmail.com>
2021-03-18 09:17:33 +0530

    Splashscreen

---
+++ lib/ui/splashscreen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wikipedia_app/ui/homepage.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    navigateToLoginScreen();
+  }
+
+  void navigateToLoginScreen() {
+    Future.delayed(
+        Duration(seconds: 3),
+            () {
+          Navigator.push(context, MaterialPageRoute(
+              builder: (context) => HomePage()));
+        }
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp
+    ]);
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.black,
+        child: Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            image: DecorationImage(
+                image: NetworkImage(
+                    "https://i.ytimg.com/vi/oVFPW0r4jWk/hqdefault.jpg")),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
